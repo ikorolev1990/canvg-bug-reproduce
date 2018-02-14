f841cfebf78066e6de216464c72bea8de00afcdf
Ilya Korolev <ikorolev@issart.com>
2018-02-14 15:38:35 +0600

    my first commit

---
+++ boot.js
@@ -0,0 +1,17 @@
+﻿var bootstrap;
+
+function main()
+{
+    var svg = $("svg")[0];
+	canvas = document.createElement("canvas");
+	canvas.height = svg.height.baseVal.value;
+	canvas.width = svg.width.baseVal.value;
+	xml = (new XMLSerializer()).serializeToString(svg);
+	canvg(canvas, xml, {scaleWidth: canvas.width, scaleHeight: canvas.height, ignoreDimensions: true});
+	var png = canvas.toDataURL("image/png");
+	var image = new Image();
+	image.src = png;
+	document.body.appendChild(image);
+}
+
+$(main);
